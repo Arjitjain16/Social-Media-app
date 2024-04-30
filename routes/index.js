53cff7e8c99a89c3c0b80c55c854a7f23c63e21e
Arjitjain16 <arjitvansh16@gmail.com>
2024-05-01 02:59:25 +0530

    commit login and register route

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
